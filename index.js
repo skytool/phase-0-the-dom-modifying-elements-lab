cc7b733bc46d93fd88ed1a51616118ac2dacd307
skytool <albert.do@aol.com>
2023-05-11 18:28:35 -0500

    coding is order matter

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 // Write your code here!
+
+document.getElementById('main').remove();
+
+
+
+const newHeader = document.createElement('h1');
+
+
+newHeader.id = 'victory';
+newHeader.innerHTML = "Albert is the champion";
+
+Element.append(newHeader);
